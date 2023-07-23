3b95518d2c047facf4e3fc0f97e4c9254118feb4
jook-bob2 <qwefk123@naver.com>
2023-07-23 22:03:53 +0900

    이미지갤러리 주석 추가

--- src/pages/example/img-gallery.jsx
+++ src/pages/example/img-gallery.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import gsap from "gsap";
 import React, { useEffect, useState } from "react";
 import "../../styles/example/img-gallery.css";
@@ -5,36 +6,59 @@ import "../../styles/example/img-gallery.css";
 export default function ImgGallery() {
   const [selectedMenu, setSelectedMenu] = useState(0);
 
+  /**
+   * @desc 렌더링 작업
+   */
   useEffect(() => {
     init();
   }, []);
 
+  /**
+   * @desc 초기화 함수
+   */
   const init = () => {
     showImg(0);
   };
 
+  /**
+   * @desc 이미지 클릭 시 동작하는 함수
+   * @param {*} index
+   */
   const imgMenuClick = (index) => {
     imgMenuActivate(index);
     showImg(index);
   };
 
+  /**
+   * @desc 이미지 인덱스 저장
+   * @param {*} index
+   */
   const imgMenuActivate = (index) => {
     setSelectedMenu(index);
   };
 
+  /**
+   * @desc 새로운 이미지를 보여주는 함수.
+   * @param {*} index
+   */
   const showImg = (index) => {
     const mainImgWrap = document.querySelector("#main");
     const newImg = document.createElement("img");
+    // 새 이미지 저장
     newImg.src = `/images/photo${index + 1}.jpg`;
 
+    // 새 이미지를 맨 앞에 이미지 저장
     mainImgWrap.prepend(newImg);
 
+    // 마지막 이미지
     const lastImage = mainImgWrap.children[1];
+    // 마지막 이미지를 제거하면서 애니메이션 동작
     gsap.to(lastImage, {
       opacity: 0,
       duration: 0.3,
       ease: "power1.out",
       onComplete: () => {
+        // 완료 시 이전 이미지 삭제
         mainImgWrap.removeChild(lastImage);
       },
     });
